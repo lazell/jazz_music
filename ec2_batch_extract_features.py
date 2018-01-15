9ae312ab70c698bc14c2c0e8d7803cbe472af99e
Katie Lazell-Fairman <katie.lazell@gmail.com>
2018-01-15 14:25:25 -0800

    fixed ec2_batch_extract_features

--- ec2_batch_extract_features.py
+++ ec2_batch_extract_features.py
@@ -1,21 +1,23 @@
 import numpy as np
+import pandas as pd
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
 import os
 
-from ec2_wav_pipeline import create_directories_and_text_files, download_mps, get_list
+from ec2_wav_pipeline import create_directories_and_text_files, download_mps
 #from download_mp3_ec2 import transfer_df_to_s3
 
 
-def get_mp3_features(filename, count):
+def get_mp3_features(filename):
     '''Generates the following for each mp3 song:
         - tempo (harmonic, precussive)
         - beats (harmonic, precussive)
         - root mean square energy per segment (mean, median, std)
 
     '''
+    print "filenam 2:", filename
     try:
         # Load audio data
         y, sr = librosa.load(filename)
@@ -37,22 +39,30 @@ def get_mp3_features(filename, count):
 
 
         # Append results to csv
-        with open('mp3_audio_features-{}.csv'.format(str(count).zfill(4)), 'a+') as f:
+        # with open('mp3_audio_features-{}.csv'.format(str(count).zfill(4)), 'a+') as f:
+        #
+        #     for feature in [filename[:-4],h_tempo, h_beats, p_tempo,p_beats ,avg_rmse ,med_rmse ,std_rmse, song_duration]:
+        #         f.write("{},".format(feature))
+        #         f.write("/n")
 
-            for feature in [filename[:-4],h_tempo, h_beats, p_tempo,p_beats ,avg_rmse ,med_rmse ,std_rmse, song_duration]:
-                f.write("{},".format(feature))
-                f.write("/n")
 
-        return y, src
+        # Generate results in dataframe
+        cols = ['filename','h_tempo', 'h_beats', 'p_tempo' ,'p_beats' ,'avg_rmse' ,'med_rmse' ,'std_rmse', 'song_duration']
+        df_audio_features = pd.DataFrame(columns = cols)
+        df_values = pd.DataFrame([filename[:-4],h_tempo, len(h_beats), p_tempo, len(p_beats) ,avg_rmse ,med_rmse ,std_rmse, song_duration]).transpose()
+        df_values.columns = cols
+
+        return y, sr, df_values
 
     except:
         print "{} was not able to be analyzed, data not saved".format(filename[:-4])
 
 
-def save_chroma_data(filename, y, sr, count):
+
+def get_chroma_data(filename, y, sr):
 
     # Get Pitch percentages of song
-    chroma = librosa.feature.chroma_cqt(y=y_2, sr=sr_2)
+    chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
 
     #Get chroma & reduce noise by using nearest neighbors with cosine similarity to reduce noise
     chroma_med = librosa.decompose.nn_filter(chroma,
@@ -71,49 +81,73 @@ def save_chroma_data(filename, y, sr, count):
     # Return pitch percentage of prominance (sum of values for each pitch over sum of all pitches)
     pitch_values = values / sum(values)
 
-    with open('mp3_audio_features_pitch-{}.csv'.format(str(count).zfill(4)),"a+") as f:
-        f.write("filename, h_tempo, h_beats, p_tempo, p_beats, avg_rmse ,med_rmse ,std_rmse, song_duration\n")
-        for feature in [filename[:-4],h_tempo, h_beats, p_tempo,p_beats ,avg_rmse ,med_rmse ,std_rmse, song_duration]:
-            f.write("{},".format(feature))
-            f.write("/n")
+    # Generate Dataframe of pitch features for song
+    df_pitch = pd.DataFrame(pitch_values).transpose()
+    df_pitch.columns = pitch_scale
+    df_pitch['filename'] = None
+    df_pitch['chroma_arr'] = None
+    df_pitch['filename'].iloc[0] = filename
+    df_pitch['chroma_arr'].iloc[0] = chroma_nlm
+
+    return df_pitch
 
 
 
 
 if __name__ == '__main__':
-    csv_list = str(raw_input("enter csv list of filenames:"))
+
     count = 1
     # Create directories
     create_directories_and_text_files()
 
     # Enter download details
     bucket_name = str(raw_input("Enter bucket name:"))
-    csv = str(raw_input("Enter csv list to download:")) #in jazz_mmusic directory list of all mp3 files to download from S3
-    start = 0
+    csv_list = str(raw_input("Enter csv list to download:")) #in jazz_mmusic directory list of all mp3 files to download from S3
+    start = 4
     stop = 3
+
     cont = 'y'
     while cont == 'y':
         # Download mp3s
-        downloaded_mp3s = download_mps(csv, bucket_name, start, stop)
+        downloaded_mp3s = download_mps(csv_list, bucket_name, start, stop)
+        os.system("")
 
-        # Navigate to music_downloads folder and generate list of files
-        get_list('music_downloads', 'mp3s.txt')
+        #Initialize dataframe
+        chroma_cols = ['filename','B','B#', 'A', 'G#', 'G', 'F#', 'E', 'D#', 'D', 'C# ','chroma_arr']
+        df_c = pd.DataFrame(columns=chroma_cols)
 
         #Get Audio Features
         with open('mp3s.txt', 'r') as f:
             for i, filename in enumerate(f):
-                while (i >= start) & (i < stop):
+                print "filename:", filename
+                if (i >= start) & (i < stop):
                     try:
-                        y, src = get_mp3_features(filename, count)
+                        y, sr, df_audio_features= get_mp3_features(musfilename)
+                        print "mp3 featues work"
+                        df_pitch = get_chroma_data(filename, y, sr)
+                        print " chroma works"
+                        #Add data to dataframe
+                        df_c = df_c.append(df_pitch)
+                        df_c = df_c.merge(df_values, on='filename')
+
                     except:
-                        print "{} did not convert".format(line)
-        #Upload Audio Feature csv to cloud & remove from local
-        # transfer_df_to_s3(downloaded_mp3s)
+                        print "{} did not convert".format(filename)
+
+
+
+        # Collate & Save DataFrame
+        df_c.to_pickle('mp3_audio_features_{}.pkl'.format(str(count).zfill(4)))
+
+        # Upload Audio Feature csv to cloud & remove from local
+        os.system('aws s3 cp mp3_audio_features_{}.pkl s3://{}/processed_data/mp3_audio_features_{}.pkl'.format(str(count).zfill(4), bucket_name, str(count).zfill(4)))
         # delete_mp3_from_local(downloaded_mp3s)
+        os.system(' cd ..')
+        #os.system('rm music_downloads')
+
         count +=1
         cont = raw_input("Continue to download next batch? (y/n)")
         if cont == 'y':
             start = stop
-            stop += 250
+            #stop += 250
         else:
             break
