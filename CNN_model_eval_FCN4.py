e844d87573a3c52b18fa708650e5b188862c2269
Katie Lazell-Fairman <katie.lazell@gmail.com>
2018-01-16 15:12:19 -0800

    updated maxpool sizes in CNN

--- CNN_model_eval_FCN4.py
+++ CNN_model_eval_FCN4.py
@@ -95,19 +95,19 @@ def Model(num_classes, input_shape):
 
     #Hidden Layer 1
     model.add(Conv2D(384, (3, 3), activation='relu', kernel_initializer='he_normal'))
-    model.add(MaxPooling2D(pool_size=(2, 5)))
+    model.add(MaxPooling2D(pool_size=(2, 4)))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
     #Hidden Layer 2
     model.add(Conv2D(768, (3, 3), activation='relu', kernel_initializer='he_normal'))
-    model.add(MaxPooling2D(pool_size=(2, 8)))
+    model.add(MaxPooling2D(pool_size=(2, 4)))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
     #Hidden Layer 3
     model.add(Conv2D(2048, (1, 1), activation='relu', kernel_initializer='he_normal'))
-    model.add(MaxPooling2D(pool_size=(2, 8)))
+    model.add(MaxPooling2D(pool_size=(2, 4)))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
