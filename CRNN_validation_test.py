0476f9bd978def25a941f134e3f72e9b46709a4b
Katie Lazell-Fairman <katie.lazell@gmail.com>
2018-02-12 22:39:35 -0500

    Cleaned up formatting and comments

---
+++ CRNN_validation_test.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.utils import shuffle
+from keras.models import model_from_json
+from sklearn import metrics
+
+'''
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Fetches Convolutional Neural Net model (for Lindy, Balboa, Shag, Charleston)
+and validates model with reserved test dataset
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+'''
+
+
+
+def scale_range(arrays, min_, max_):
+    '''
+     Standardizes input data between two user specified values
+      INPUT:
+            -  arrays : array(s)
+            -  min_   : minumum scale value (int)
+            -  max_   : maximum scale value (int)
+      OUTPUT:
+            - arrays  : re-scaled array(s)
+
+      '''
+    arrays += -(np.min(arrays))
+    arrays /= np.max(arrays) / (max_ - min_)
+    arrays += min_
+
+    return arrays
+
+def load_validation_data():
+
+    ''' Loads and pre-processes validation X and y data
+
+        OUTPUT:
+              - X_test   : scaled test mel spectrogram arrays (array)
+              - y_test   : test labels (array)
+              - df       : test metadats (dataframe)
+    '''
+    fpath = '/Users/katielazell-fairman/desktop/projects/jazz_music/'
+
+    # Load song metadata dataframe
+    df = pd.read_csv(fpath+'processed_data/processed_metadata10.csv')
+
+    # Load processed mel-spectrogram arrays
+    arr_test = (np.load(fpath+'processed_data/processed_X_data10.npz')
+                        ['arr_0'][1900:2100])
+    # Load binerized song labels
+    y_test = (np.load('processed_data/processed_Y_sty_data10.npz')
+                        ['arr_0'][1900:2100])
+
+    # Use Standardizing array values to fit normalization
+    std_arr = (np.load(fpath+'processed_data/standardizing_X_array10.npz')
+                        ['arr_0'])
+    std = np.expand_dims(std_arr, axis=0)
+
+    # Combine Stardardizing array with mel-spectrograms
+    arr_test = arr_test.reshape((200, 128, 1292))
+    std_and_arr = np.vstack([std,arr_test])
+
+    # Normalize/Scaled between -1 and 1
+    std_and_arr_scaled = scale_range(std_and_arr,-1,1)
+
+    # After scaling remove standardizing Array and re-dimension
+    X = std_and_arr_scaled[1:,:,:]
+    X_test = X.reshape((200, 128, 1292,1))
+
+    # Shuffle test set
+    X_test = shuffle(X_test, random_state=14)
+    y_test = shuffle(y_test, random_state=14)
+
+
+    return X_test, y_test, df
+
+
+def crnn(X_test):
+
+    '''  Convolutional Neural Net: predicts for Lindy, Balboa, Shag, Charleston
+
+
+        INPUT:  arr_test     :   Array of pre-normalized mel-spectrograms
+                                 (dimensions n x 22) for testing
+
+        OUTPUT: df_pred_crnn :   Dataframe of predictions
+                                 (dimensions m x 4) rows in format:
+                                 [Lindy, Balboa, Charleston, Shag, display_text]
+        '''
+
+    # Create CRNN Model from saved json file
+    fpath ='/Users/katielazell-fairman/desktop/projects/jazz_music/CNN_Models/'
+    with open(fpath+'model-0123024.json', 'r') as json_file:
+        loaded_model_json = json_file.read()
+        # load model
+        crnn_model = model_from_json(loaded_model_json)
+
+    # LOoad weights into new model
+    crnn_model.load_weights(fpath+"model-0123024.h5")
+    print("Loaded model with weights")
+
+    # Compile & evaluate loaded model on test data
+    crnn_model.compile(loss='categorical_crossentropy',
+                       optimizer='adam',
+                       metrics=['accuracy'])
+
+    y_pred_crnn = crnn_model.predict(X_test)
+
+    # Save predictuions to DataFrame
+    df_pred_crnn = pd.DataFrame(y_pred_crnn)
+    df_pred_crnn.columns = ['Lindy', 'Balboa', 'Charleston', 'Shag']
+
+    return df_pred_crnn
+
+if __name__ == '__main__':
+    # Load validation data
+    X_test, y_test, df = load_validation_data()
+    print "loading data to model..."
+
+    # Generate validationpredictions dataframe
+    df_pred_crnn = crnn(X_test)
+
+    # Print accuracy score
+    y_pred_arr = np.around(np.array(df_pred_crnn),decimals =1)
+    accuracy = ((y_test + y_pred_arr) > 1.3).sum()*1./len(y_test)
+    print "\n CRNN Model accuracy: {}\n\n".format(round(accuracy,3))
+
+    # Print prediction results (up to first 30) if probability > 25%
+    for i, x in enumerate(y_pred_arr[:30]):
+        print "\n\nsong: {}\n".format(i)
+        if df_pred_crnn.iloc[i][0] > 0.25:
+            print "Lindy",round(df_pred_crnn.iloc[i][0],2)*100, "%"
+
+        if df_pred_crnn.iloc[i][1] > 0.25:
+            print "Balboa ",round(df_pred_crnn.iloc[i][1],2)*100, "%"
+
+        if df_pred_crnn.iloc[i][2] > 0.25:
+            print "Shag", round(df_pred_crnn.iloc[i][2],2)*100, "%"
+
+        if df_pred_crnn.iloc[i][3] > 0.25:
+            print "Charleston", round(df_pred_crnn.iloc[i][3],2)*100, "%"
--- get_swing_prediction.py
+++ get_swing_prediction.py
@@ -1,37 +1,71 @@
-import os
+
 import numpy as np
 import pandas as pd
-import librosa
-import librosa.display
-from  pydub import AudioSegment
 import matplotlib.pyplot as plt
 import pickle
 
+import librosa
+import librosa.display
+from pydub import AudioSegment
+
+
 from mp3_sampling import song_samples
-from CRNN_ensemble_model import scale_range, crnn
-from RF_KNN_ensemble_model import check_if_lindy, check_if_blues, k_nearest_neighbor
+from CRNN_validation_test import scale_range, crnn
+from RF_KNN_ensemble_model import check_if_lindy, check_if_blues
+from RF_KNN_ensemble_model import k_nearest_neighbor
 from playlist_generator import generate_playlist
 
+'''
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+~*~*~* Lindy List *~*~*~
 
-def download_mp3_youtube(url):
-    os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(url))
+Processes features and mel spectrogram array for a single song (mp3) and
+returns the recommended swing-dance style  based on the selected
+machine learning model
+
+OPTIONS:
+
+ 1. Ensemble model (e)
+    Predicts single style of dance from: Lindy, Blues, Balboa, Shag, Charleston
+
+ 2. Convolutional Recurrent Neural Net (nn)
+    Predicts percentages for Lindy, Balboa, Shag, Charleston styles of dance
+    (returns only those with 0.25 style probability or greater)
+
+ 3. Generate Dance Playlist (y/n)
+    Provides sample 30-minute playlist for one of the predicted dance styles
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+'''
+
 
 
 def get_mp3_audio_features(filename):
-    '''Generates the following for each mp3 song:
+    '''
+
+    Generates the following audio features for each song:
         - tempo (harmonic, precussive)
         - beats (harmonic, precussive)
         - root mean square energy per segment (mean, median, std)
-        - song duration
-        - pitch prominance values 'B','A#', 'A', 'G#', 'G', 'F#','F', 'E' etc.
+        - song duration (seconds)
         - engineered tempo fearures
+
+    INPUT :
+            - filename   : filepath to single song file (.mp3 or .wav)
+
+    OUTPUT :
+            - audio_data : list of 10 extract audio features
+            - y          : audio time series (ndarray)
+            - sr         : sampling rate of y (float)
+
     '''
     print "Processing: " + filename + " this may take a while ..."
+
     # Load audio data
     y, sr = librosa.load(filename)
     print "loading file.."
 
-    # Get Tempo & Beats
+    # Get Harmonic and Percussive Tempo & Beats
     y_harmonic, y_percussive = librosa.effects.hpss(y)
     h_tempo, h_beats = librosa.beat.beat_track(y=y_harmonic, sr=sr)
     p_tempo, p_beats = librosa.beat.beat_track(y=y_percussive, sr=sr)
@@ -67,6 +101,20 @@ def get_mp3_audio_features(filename):
     return audio_data, y, sr
 
 def get_mp3_pitch_features(filename, y, sr):
+
+    '''
+    Generates 12 pitch features (B, A#, A, G#, G, F#, F, E etc.) for each song
+
+        INPUT :
+                - filename   : filepath to single song file ( .mp3 or .wav)
+                - y          : audio time series (ndarray)
+                - sr         : sampling rate of y (float)
+
+        OUTPUT :
+                - pitch_values : list of 10 extract audio features
+
+    ''''
+
     # Get Pitch percentages of song
     chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
     print "got chroma..."
@@ -77,18 +125,19 @@ def get_mp3_pitch_features(filename, y, sr):
                                              aggregate=np.median,
                                              metric='cosine')
     rec = librosa.segment.recurrence_matrix(chroma, mode='affinity',
-                                            metric='cosine', sparse=True)
+                                            metric='cosine',
+                                            sparse=True)
     chroma_nlm = librosa.decompose.nn_filter(chroma, rec=rec,
                                              aggregate=np.average)
     print "got chroma_nlm..."
 
-    pitch_scale = ['B','A#', 'A', 'G#', 'G', 'F#','F', 'E', 'D#', 'D','C#','C']
+    pitch_scale = ['B','A#','A','G#','G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
     values = []
 
     for i, pitch in enumerate(pitch_scale):
         values.append(chroma_nlm[i].sum())
     # Return pitch percentage of prominance (sum of values for each pitch over
-    # sum of all pitches)
+    # sum of all pitches
     pitch_values = values / sum(values)
 
     return pitch_values
@@ -99,20 +148,22 @@ def create_X_data(audio_data, pitch_values,filename):
 
     df_X =  pd.DataFrame([X])
     df_X.columns = ['h_tempo', 'h_beats', 'p_tempo', 'p_beats',
-                  'rmse mean','rmse median', 'rmse std', 'duration',
-                  'tempo-differ','slow-tempo-correction', 'B', 'A#', 'A',
-                  'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
+                    'rmse mean', 'rmse median', 'rmse std', 'duration',
+                    'tempo-differ', 'slow-tempo-correction', 'B', 'A#', 'A',
+                    'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
     df_X['filename'] = filename
+
     return df_X
 
 def mel_spectrogram(filename):
-    # Generate Mel-scaled power (energy-squared) spectrogram
+    # Generate Mel-scaled power (energy-squared) spectrogram @ 128 mels
     y, sr = librosa.load(filename)
     S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
 
     # Convert to log scale (dB) with peak power (max) as reference.
     log_S = librosa.power_to_db(S, ref=150)
-    plt.figure(figsize=(12,4))
+    plt.figure(figsize=(12, 4))
+
     # Display the spectrogram on a mel scale
     librosa.display.specshow(log_S)
     plt.show()
@@ -125,19 +176,19 @@ def mel_spectrogram(filename):
     return log_S
 
 def prep_array(arr):
-
     # Use Standardizing array to fit normalization
     std_arr = (np.load('processed_data/standardizing_X_array10.npz')
-                        ['arr_0'])
+                      ['arr_0'])
     std = np.expand_dims(std_arr, axis=0)
+
     # Combine arrays
     arr = arr.reshape((1, 128, 1292))
     std_and_arr = np.vstack([std, arr])
-    std_and_arr_scaled = scale_range(std_and_arr,-1,1)
+    std_and_arr_scaled = scale_range(std_and_arr, -1, 1)
 
     # After scaling remove stand scaler Array and redimension
-    X = std_and_arr_scaled[1:,:,:]
-    X_arr = X.reshape((1,128, 1292,1))
+    X = std_and_arr_scaled[1:, :, :]
+    X_arr = X.reshape((1, 128, 1292, 1))
     return X_arr
 
 def predict_from_ensamble_model(df_X):
@@ -146,18 +197,18 @@ def predict_from_ensamble_model(df_X):
     knn_model = pickle.load(open('CNN_Models/knn_model.pkl', 'rb'))
 
     blues_cols = ['h_tempo', 'h_beats', 'p_tempo', 'p_beats',
-                  'tempo-differ','duration','slow-tempo-correction', 'rmse mean',
+                  'tempo-differ', 'duration', 'slow-tempo-correction',
+                  'rmse mean', 'rmse median', 'rmse std', 'B', 'A#', 'A',
+                  'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
+
+    lindy_cols = ['h_tempo', 'h_beats', 'p_tempo', 'p_beats',
+                  'tempo-differ', 'slow-tempo-correction', 'rmse mean',
                   'rmse median', 'rmse std', 'B', 'A#', 'A', 'G#', 'G',
                   'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
 
-    lindy_cols = ['h_tempo', 'h_beats', 'p_tempo', 'p_beats',
-                      'tempo-differ', 'slow-tempo-correction', 'rmse mean',
-                      'rmse median', 'rmse std', 'B', 'A#', 'A', 'G#', 'G',
-                      'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
-
     knn_cols = ['h_tempo', 'h_beats', 'p_tempo', 'p_beats', 'rmse mean',
-                      'rmse median', 'rmse std', 'B', 'A#', 'A', 'G#', 'G',
-                      'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
+                'rmse median', 'rmse std', 'B', 'A#', 'A', 'G#', 'G',
+                'F#', 'F', 'E', 'D#', 'D', 'C#', 'C']
 
     y_pred_lindy = lindy_rf_model.predict(df_X[lindy_cols])
     df_pred_lindy = pd.DataFrame(y_pred_lindy)
@@ -171,13 +222,17 @@ def predict_from_ensamble_model(df_X):
     df_pred_knn = pd.DataFrame(y_pred_knn)
     df_pred_knn.columns = ['knn']
 
-    # Ensamble Results Table
-    df_results = pd.merge(df_pred_lindy, df_pred_blues, how='outer', left_index=True, right_index=True)
-    df_results = pd.merge(df_results, df_pred_knn, how='outer', left_index=True, right_index=True)
+    # Merge to single ensamble results table
+    df_results = pd.merge(df_pred_lindy, df_pred_blues,
+                          how='outer', left_index=True, right_index=True)
+    df_results = pd.merge(df_results, df_pred_knn,
+                          how='outer', left_index=True, right_index=True)
 
 
     df_results['Dance Prediction'] = 0
-    for i, (x, y, z) in enumerate(zip(df_results['Lindy_?'], df_results['Blues_?'], df_results['knn'])):
+    for i, (x, y, z) in enumerate(zip(df_results['Lindy_?'],
+                                      df_results['Blues_?'],
+                                      df_results['knn'])):
         df_results['Dance Prediction'].iloc[i] = z
         if x == 1:
             df_results['Dance Prediction'].iloc[i] = 'Lindy'
@@ -188,38 +243,36 @@ def predict_from_ensamble_model(df_X):
 
 
 if __name__ == '__main__':
-    df_songs = pd.read_csv('music_downloads/mp3_song_master_for_RF_KNN_model.csv')
-    new = str(raw_input('\nWould you like to sample a new song? (y/n):'))
-    if new == 'y':
-        url = str(raw_input('Enter url link:'))
-        download_mp3_youtube(url)
+    df_songs = pd.read_csv('''music_downloads/
+                              mp3_song_master_for_RF_KNN_model.csv''')
 
-    filename = "Demo_testing/"+str(raw_input('Locate mp3 and copy filename:')
+    filename = "Demo_testing/"+str(raw_input('Enter filepath of mp3:')
                                    )
     model = str(raw_input('\nRun ensemble model or neural net? (e/nn):'))
-    if model == 'e':
+
+    # Ensemble Model
+    if model == "e":
         audio_data, y, sr = get_mp3_audio_features(filename)
         pitch_values = get_mp3_pitch_features(filename, y, sr)
         df_X = create_X_data(audio_data,pitch_values,filename)
         style = predict_from_ensamble_model(df_X)
         print "\n\nsong: {}\n".format(filename)
+        # Print recommended dance style
         print "Recommended dance style: ", style, "\n\n"
 
-
-
-    if model == 'nn':
-        wav_filename  = song_samples(1,30,filename,skip=20)
+    # Neural Network Model
+    if model == "nn":
+        wav_filename = song_samples(1, 30, filename, skip=20)
         array = mel_spectrogram(wav_filename)
-        array_reshaped = array.reshape((128,1292,1))
+        array_reshaped = array.reshape((128, 1292, 1))
         X_arr = prep_array(array_reshaped)
         df_pred_crnn = crnn(X_arr)
         y_pred_arr = np.around(np.array(df_pred_crnn),decimals =1)
 
-
-        # Print results (up to first 30)
+        # Print results for predictions with probability over 25%
         for i, x in enumerate(y_pred_arr[:30]):
             print "\n\nsong: {}\n".format(wav_filename)
-            print "Recommended dance style: "
+            print "Recommended dance style(s): "
             if df_pred_crnn.iloc[i][0] > 0.25:
                 print "Lindy",round(df_pred_crnn.iloc[i][0],2)*100, "%"
                 style = "Lindy"
@@ -237,6 +290,9 @@ if __name__ == '__main__':
                 style = "Charleston"
             print "\n\n"
 
-    style_playlist = str(raw_input("Would you like to see a playlist for {} style? (y/n):".format(style)))
+    style_playlist = str(raw_input('''Would you like to see a playlist for
+                                      {} style? (y/n):'''.format(style)))
     if style_playlist == 'y':
-        print " \n\n ----  Playlist for {} Dance Style  ----- \n\n {}".format(style, generate_playlist(30,style,df_songs))
+        print ''' \n\n ----  Playlist for {} Dance Style  -----
+                  \n\n {}'''.format(style,
+                                    generate_playlist(30, style, df_songs))
