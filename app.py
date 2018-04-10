4423c2056309911af0c2cf6bf43d4ddf8ab2a22f
Katie Lazell-Fairman <katie.lazell@gmail.com>
2018-04-09 20:31:38 -0400

    Initial commit flask web app

---
+++ app.py
@@ -0,0 +1,41 @@
+
+
+from flask import Flask, render_template, requests
+import numpy as np
+import keras.models
+import sys
+import os
+
+
+'''
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Web-App for Fetches Convolutional Neural Net model (for Lindy, Balboa, Shag, Charleston)
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+'''
+from
+
+
+app = Flask(__name__)
+
+global model, graph
+model, graph = init()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    youtubelink = requests.get_data()
+    x = to_array(youtubelink)
+    # look-up download you tube link
+
+    with graph.as_default():
+        out = model.ppredict(x)
+        response = np.array_str(np.argmax(out))
+        return response
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
